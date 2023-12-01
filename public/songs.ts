e77615b07f656bf6786acafcf722837dae4e2cc4
guaru345 <abelmtz34@outlook.com>
2023-12-01 10:06:38 -0600

    add json of song tracks

---
+++ public/songs.ts
@@ -0,0 +1,102 @@
+export const STREAMSONGS = [
+  {
+    name: "BLOOD",
+    track: "songs/BLOOD.mp3",
+  },
+  {
+    name: "DNA",
+    track: "songs/DNA.mp3",
+  },
+  {
+    name: "ELEMENT",
+    track: "songs/ELEMENT.mp3",
+  },
+  {
+    name: "FEEL",
+    track: "songs/FEEL.mp3",
+  },
+  {
+    name: "DUCKWORTH",
+    track: "songs/DUCKWORTH.mp3",
+  },
+  {
+    name: "Over Now",
+    track: "songs/OverNow.mp3",
+  },
+  {
+    name: "rockstar",
+    track: "songs/rockstar.mp3",
+  },
+  {
+    name: "Stay",
+    track: "songs/Stay.mp3",
+  },
+  {
+    name: "Psycho",
+    track: "songs/Psycho.mp3",
+  },
+  {
+    name: "Better Now",
+    track: "songs/BetterNow.mp3",
+  },
+  {
+    name: "In Your Eyes",
+    track: "songs/InYourEyes.mp3",
+  },
+  {
+    name: "Save Your Tears",
+    track: "songs/SaveYourTears.mp3",
+  },
+  {
+    name: "Blinding Lights",
+    track: "songs/BlindingLights.mp3",
+  },
+  {
+    name: "Faith",
+    track: "songs/Faith.mp3",
+  },
+  {
+    name: "Heartless",
+    track: "songs/Heartless.mp3",
+  },
+  {
+    name: "Gold",
+    track: "songs/Gold.mp3",
+  },
+  {
+    name: "Shots",
+    track: "songs/Shots.mp3",
+  },
+  {
+    name: "Hopeless Opus",
+    track: "songs/HopelessOpus.mp3",
+  },
+  {
+    name: "Dream",
+    track: "songs/Dream.mp3",
+  },
+  {
+    name: "Who We Are",
+    track: "songs/WhoWeAre.mp3",
+  },
+  {
+    name: "Y LLORO",
+    track: "songs/YLLORO.mp3",
+  },
+  {
+    name: "MIENTRAS DUERMES",
+    track: "songs/MIENTRASDUERMES.mp3",
+  },
+  {
+    name: "$AD BOYZ II",
+    track: "songs/$ADBOYZII.mp3",
+  },
+  {
+    name: "ROCKSTAR",
+    track: "songs/JHROCKSTAR.mp3",
+  },
+  {
+    name: "SERPIENTE",
+    track: "songs/SERPIENTE.mp3",
+  },
+];
