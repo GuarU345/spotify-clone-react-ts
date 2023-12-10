ba166f7d31ae74a9045d436b3a02be75736b1bbb
GuarU345 <95324317+GuarU345@users.noreply.github.com>
2023-12-10 02:31:02 -0600

    show the playlist user and liked albums in same component

--- src/components/aside/SideMenuItemCard.tsx
+++ src/components/aside/SideMenuItemCard.tsx
@@ -1,10 +1,10 @@
+import { useFetchLikedData } from "../../hooks/useFetchUserLikedData";
 import { useAuthStore } from "../../store/useAuthStore";
-import { useFetchLikedAlbums } from "../../hooks/useFetchAlbums";
 import { Link } from "react-router-dom";
 
 export const SideMenuItemCard = () => {
   const { token } = useAuthStore();
-  const { data, error, isLoading } = useFetchLikedAlbums(token);
+  const { data, error, isLoading } = useFetchLikedData(token);
 
   return (
     <>
@@ -14,19 +14,28 @@ export const SideMenuItemCard = () => {
         </>
       )}
       <ul className="flex flex-col gap-2">
-        {data?.map((album) => (
-          <Link key={album.id} to={`/album/${album.id}`}>
+        {data?.map((likedData) => (
+          <Link
+            key={likedData.name}
+            to={
+              likedData.type === "album"
+                ? `/album/${likedData.id}`
+                : `/playlist/${likedData.id}`
+            }
+          >
             <li className="flex items-center  p-2 gap-2 hover:cursor-pointer hover:bg-white/5">
               <picture>
                 <img
                   className="w-16 h-16 rounded-md"
-                  src={album.image}
-                  alt={album.name}
+                  src={likedData.image}
+                  alt={likedData.name}
                 />
               </picture>
               <div className="flex flex-col text-white">
-                <p className="text-sm font-semibold">{album.name}</p>
-                <span className="text-xs text-gray-300">{album.artist}</span>
+                <p className="text-sm font-semibold">{likedData.name}</p>
+                <span className="text-xs text-gray-300">
+                  {likedData.author}
+                </span>
               </div>
             </li>
           </Link>
--- src/hooks/useFetchAlbums.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "react-query";
-import { AlbumsService } from "../services/albums";
-
-export const useFetchLikedAlbums = (token) => {
-  return useQuery("likedAlbums", () => {
-    return AlbumsService.getLikedAlbums(token);
-  });
-};
